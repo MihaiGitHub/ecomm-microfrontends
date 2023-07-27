eefc70e8f08475a92a737d7095597f8bf5a3c2f3
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-26 21:02:08 -0700

    Refactor Products app

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,12 +1,27 @@
 import faker from "faker";
 
-let products = "";
+// take in a reference to an HTML element
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
 
-  products += `<div>${name}</div>`;
+    products += `<div>${name}</div>`;
+  }
+
+  // when this code is run inside the Container, the ID needs to be present inside the container as well
+  el.innerHTML = products;
+};
+
+// process is environment variable set by webpack
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    // if inside this if, then we are running this Remote in isolation, not in Container app
+    mount(el);
+  }
 }
 
-// when this code is run inside the Container, the ID needs to be present inside the container as well
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
